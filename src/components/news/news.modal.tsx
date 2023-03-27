ccb315f61c055f0f7de0ecc43a2f4075e6443711
Arek17 <daszkowskiarek145@gmail.com>
2023-03-27 19:23:05 +0200

    create a popup with news

---
+++ src/components/news/news.modal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Link } from '@material-ui/core';
+import type { NewsPopupProps } from './news.types'
+
+
+const NewsPopup: React.FC<NewsPopupProps> = ({ author, content, url, isOpen, onClose }) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <DialogTitle>{author}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{content}</DialogContentText>
+        <DialogContentText>
+          <Link href={url} target="_blank" rel="noopener">
+            {url}
+          </Link>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default NewsPopup;
