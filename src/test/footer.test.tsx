25cf7ae9307ab66b21e90ae6ba9fc9ba12eaf480
Arek17 <daszkowskiarek145@gmail.com>
2023-03-30 23:10:53 +0200

    footer test

---
+++ src/test/footer.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import Footer from '../components/footer/footer';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import '@testing-library/jest-dom/extend-expect';
+
+const mockStore = configureStore([]);
+
+describe('Footer', () => {
+  it('should display current date and time and news count', () => {
+    const store = mockStore({
+      news: { news: [{ title: 'News 1' }, { title: 'News 2' }] },
+    });
+
+    render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
+
+    expect(screen.getByText(/news articles/i)).toBeInTheDocument();
+    expect(screen.getByText(/news articles/i)).toHaveTextContent('2 news articles');
+  });
+});
